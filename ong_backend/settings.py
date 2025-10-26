a79640a0a15613063f81ef543f62e80e6746cdd2
Marlon-Tumamak <tumamak.mariomarlon@gmail.com>
2025-10-27 07:21:52 +0800

    added some changes and filters

--- ong_backend/settings.py
+++ ong_backend/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'ong_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite5',
+        'NAME': BASE_DIR / 'db.sqlite3',
         # 'USER': 'postgres',
         # 'PASSWORD': 'Gwapo123',
         # 'HOST': 'localhost',
